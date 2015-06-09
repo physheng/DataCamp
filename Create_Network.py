2292f00c8128c3fc400a60df34c5462c5b15f416
physheng <phyangsheng@gmail.com>
2015-06-09 10:51:14 -0400

    Bug Fix
    fix bugs in create_network.py

--- Create_Network.py
+++ Create_Network.py
@@ -1,11 +1,16 @@
 # Read json file and print dictionary
 import json
+import numpy as np
+import pandas as pd
+import itertools
 
 # Load json file
 data = []
 with open('test.json') as f:
 	for line in f:
 		data.append(json.loads(line))
+
+df = pd.DataFrame(data)
 
 # Create network file
 network = open('gameNetwork.csv','w')
@@ -16,7 +21,7 @@ for i in range(len(data)):
 	elif len(data[i]['Credits']) > 1:
 		developer_pair = list(itertools.combinations(data[i]['Credits'],2))
 		for j in range(len(developer_pair)):
-			network.write("%s, %s, %s\n" %(developer_pair[j][0], developer_pair[j][1], data['year']) )
+			network.write("%s, %s, %s\n" %(developer_pair[j][0], developer_pair[j][1], data[i]['year']) )
 	else:
 		pass
 
